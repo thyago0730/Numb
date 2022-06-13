82bd812d40854f0b79cfc0a49413e8d19c3b472b
Thiago Rissati <thyago0730@gmail.com>
2022-06-13 19:07:16 -0300

    Commit Inicial

---
+++ Numb/v15.0/landing_pages/js/showMenu.js
@@ -0,0 +1,18 @@
+function mostrarMenu()
+{
+    var menu  = document.getElementById("menu-header")
+    var icone = document.getElementById("icone-menu")
+
+    if (getComputedStyle(menu).display == 'none')
+    {
+        menu.style.display = 'flex'
+        icone.classList.remove('fa-bars')
+        icone.classList.add('fa-times')
+    }
+    else
+    {
+        menu.style.display = 'none'
+        icone.classList.remove('fa-times')
+        icone.classList.add('fa-bars')
+    }
+}
